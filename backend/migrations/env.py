dcbac9a2bf86aa4416500d4ae8e898f5efa48d25
NamanKhanna27 <namank27102004@gmail.com>
2026-03-11 17:53:40 -0600

    Implement schedule linking for reservations in poller listener

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -1,10 +1,9 @@
 import os
 from logging.config import fileConfig
 
+from alembic import context
 from dotenv import load_dotenv
 from sqlalchemy import engine_from_config, pool
-
-from alembic import context
 
 load_dotenv()
 
@@ -40,9 +39,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
